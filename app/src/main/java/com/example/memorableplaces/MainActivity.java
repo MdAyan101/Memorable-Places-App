f6a3d1e1a5caab1c841d5a146cc22c9e637b9924
Mohammad Ayan <mdayanmg@gmail.com>
2021-09-16 23:53:52 +0530

    shared preferences functionality addded

--- app/src/main/java/com/example/memorableplaces/MainActivity.java
+++ app/src/main/java/com/example/memorableplaces/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.memorableplaces;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
@@ -27,15 +29,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.memorableplaces" , Context.MODE_PRIVATE);
+
+        ArrayList<String> latitudes = new ArrayList<>();
+        ArrayList<String> longitudes = new ArrayList<>();
+
+        places.clear();
+        latitudes.clear();
+        longitudes.clear();
+        locations.clear();
+
+        try{
+
+            places = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("places",ObjectSerializer.serialize(new ArrayList<>())));
+            latitudes = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("lats",ObjectSerializer.serialize(new ArrayList<>())));
+            longitudes = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("longs",ObjectSerializer.serialize(new ArrayList<>())));
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        if(places.size() > 0 && latitudes.size() >0 && longitudes.size() >0){
+            if(places.size() == longitudes.size() && places.size() == latitudes.size()){
+                for(int i=0;i<latitudes.size();i++){
+                    locations.add(new LatLng(Double.parseDouble(latitudes.get(i)) , Double.parseDouble(longitudes.get(i))));
+                }
+            }
+        }else{
+            places.add("Tap to add your place! ");
+            locations.add(new LatLng(0,0));
+        }
+
         listView = findViewById(R.id.placesListView);
-
-
-
-
-
-        places.add("Tap to add your place! ");
-        locations.add(new LatLng(0,0));
-
+        
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_checked, places);
 
         listView.setAdapter(arrayAdapter);
